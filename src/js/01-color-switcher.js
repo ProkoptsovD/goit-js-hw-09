1bffc40d0a25b0832e33354eef0cb80f0f19b3e6
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-28 13:24:52 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  intervalId: null,
+};
 
+refs.stopBtn.setAttribute('disabled', 'disabled');
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+//обрабатывает событие "клик" на кнопке startBtn
+function onStartBtnClick() {
+  refs.intervalId = setInterval(changeBodyBgColor, 1000);
+  toggleAttribute('disabled');
+}
+
+//обрабатывает событие "клик" на кнопке startBtn
+function onStopBtnClick() {
+  clearInterval(refs.intervalId);
+  toggleAttribute('disabled');
+}
+
+//изменяет фоновый цвет body
+function changeBodyBgColor() {
+  const bodyRef = document.querySelector('body');
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+//выполняет toggle атрубута disabled у кнопок startBtn и stopBtn
+function toggleAttribute(attribute) {
+  refs.startBtn.toggleAttribute(attribute);
+  refs.stopBtn.toggleAttribute(attribute);
+}
+
+//генерация случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
