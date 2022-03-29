82645cdc654b29ff3c417f033693cd6db3bea933
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-29 22:47:56 +0300

    almost ready timer :)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,22 +2,44 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 const orangeTheme = require('flatpickr/dist/themes/material_blue.css');
 
+// кастомный класс Timer
+import Timer from './Timer';
+
+//объект настроек для библиотеки flatpickr
+import { flatpickrOptions } from './flatpickr-options';
+
 const refs = {
   inputDatetimePicker: document.querySelector('#datetime-picker'),
   startCoundownBtn: document.querySelector('[data-start]'),
-  timer: document.querySelector('.timer'),
 };
 
-const flatpickrOptions = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
-
-flatpickr(refs.inputDatetimePicker, flatpickrOptions);
-
+//делает кнопку неактивной до начала выбора времени отсчета
 refs.startCoundownBtn.setAttribute('disabled', 'disabled');
+
+// создает таймер и создает поле выбора даты для библиотеки flatpickr
+const timer = new Timer();
+timer.createDatetimePicker(refs.inputDatetimePicker, flatpickr, flatpickrOptions);
+
+// узнаем выбранное время
+refs.inputDatetimePicker.addEventListener('input', e => {
+  refs.startCoundownBtn.addEventListener(
+    'click',
+    () => {
+      timer.startCoundown();
+    },
+    { once: true },
+  );
+  const selectedDatetime = Date.parse(timer.flatpickr.selectedDates[0]);
+
+  if (!timer.isDatetimeValid(selectedDatetime)) {
+    timer.onWrongDatetimePicked(refs.startCoundownBtn);
+    alert('wrong datetime');
+    return;
+  }
+
+  timer.onCorectDatetimePicked(refs.startCoundownBtn, selectedDatetime);
+});
+
+// onStartCountdownBtnClick() {
+
+// }
---
+++ src/js/flatpickr-options.js
@@ -0,0 +1,8 @@
+const flatpickrOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+};
+
+export { flatpickrOptions };
